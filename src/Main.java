169fad6bc50fd4250473dd31df979b961c59bebf
Atul <atul43653@gmail.com>
2023-03-04 17:31:44 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int a=1;
+        int b=2;
+        int sum =a+b;
+        int sub =a-b;
+        int mul =a*b;
+        int mod =a%b;
+        System.out.println("The addition" +sum);
+        System.out.println("The subtraction" +sub);
+        System.out.println("The multiplication" +mul);
+        System.out.println("The modulus" +mod);
+    }
+}
+
+
+
+
+
