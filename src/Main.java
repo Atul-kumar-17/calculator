f5c44d0ff30e4531df0d11ec2ddd5a103fb8f4bc
Atul <atul43653@gmail.com>
2023-03-04 17:50:12 +0530

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        int a=1;
-        int b=2;
-        int sum =a+b;
-        int sub =a-b;
-        int mul =a*b;
-        int mod =a%b;
-        System.out.println("The addition" +sum);
-        System.out.println("The subtraction" +sub);
-        System.out.println("The multiplication" +mul);
-        System.out.println("The modulus" +mod);
+//        System.out.println("Hello world!");
+        int a = 1;
+        int b = 2;
+        int sum = a + b;
+        int sub = a - b;
+        int mul = a * b;
+        int mod = a % b;
+//        System.out.println("The addition" +sum);
+//        System.out.println("The subtraction" +sub);
+//        System.out.println("The multiplication" +mul);
+//        System.out.println("The modulus" +mod);
+
+
+//
+        float subject1 = 43;
+        float subject2 = 64;
+        float subject3 = 55;
+        float cgpa = (subject1 + subject2 + subject3) / 30;
+        System.out.println("percentage" +cgpa);
     }
 }
 
